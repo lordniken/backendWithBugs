d98004bdc27b6ac77026de2150351b64599e575f
Alexey Malyshev <lordniken@mail.ru>
2021-10-23 14:33:15 +0300

    Changed response status

--- src/routes.js
+++ src/routes.js
@@ -2,31 +2,33 @@ const express = require('express');
 const router = express.Router();
 const products = require('./mock/products');
 
-router.get('/products', (req, res) => {
+router.get('/products', (_req, res) => {
   return res.status(200).json(products);
 });
 
 router.patch('/products/:id/rating', (req, res) => {
   /*    #swagger.parameters['obj'] = {
           in: 'body',
-          description: 'Change product rating (from 1 to 5)',
+          description: 'Change product rating (from 0 to 5)',
           schema: {
               $rating: 5,
           }
         } 
   */
   const { id } = req.params;
+  const findedProduct = products.find(product => product.id === id);
 
-  try {
-    const findedProduct = products.find(product => product.id === id);
+  if (!findedProduct) {
+    return res.status(404).json();
+  }
 
-    findedProduct.rating = req.body.rating;
-  
-    return res.status(200).json(products);  
-  } catch (error) {
+  if (Number(req.body.rating) > 5) {
     return res.status(500).json();
   }
-  
+
+  findedProduct.rating = req.body.rating;
+
+  return res.status(200).json(findedProduct);  
 });
 
 module.exports = router;
